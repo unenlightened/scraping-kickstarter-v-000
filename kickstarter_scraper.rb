f1efd4e7d04eab203cba62bc4c91348d33f26b74
Chely Ho <adept_inept@yahoo.com>
2018-02-14 01:33:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
-  # percent_funded: project.css("li.first.funded strong").text
+  # percent_funded: project.css("li.first.funded strong").text.gsub("%", "").to_i
 
 
 binding.pry
